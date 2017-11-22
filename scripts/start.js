d54b1f6bf96e0d8e8c3faf1d7b34214bf67086af
Lukas Bombach <lukas.bombach@googlemail.com>
2017-11-22 22:29:45 +0100

    Added basic electron support

--- scripts/start.js
+++ scripts/start.js
@@ -28,13 +28,13 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const clearConsole = require('react-dev-utils/clearConsole');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+const spawn = require("child_process").spawn;
 const {
   choosePort,
   createCompiler,
   prepareProxy,
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
-const openBrowser = require('react-dev-utils/openBrowser');
 const paths = require('../config/paths');
 const config = require('../config/webpack.config.dev');
 const createDevServerConfig = require('../config/webpackDevServer.config');
@@ -82,7 +82,11 @@ choosePort(HOST, DEFAULT_PORT)
         clearConsole();
       }
       console.log(chalk.cyan('Starting the development server...\n'));
-      openBrowser(urls.localUrlForBrowser);
+      const electron = spawn("electron", ["scripts/electron.js"]); // todo pass port
+      electron.on('close', () => {
+        devServer.close();
+        process.exit();
+      });
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
